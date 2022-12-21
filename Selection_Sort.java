5fa96fd7aaa3eb3cbbc94b3cf0f8bcd61b98aa10
Rishi Mittal <95878871+Rishi-mittal13@users.noreply.github.com>
2022-12-21 10:37:12 +0530

    Create Selection_Sort.java

---
+++ Selection_Sort.java
@@ -0,0 +1,26 @@
+//selection sort
+public class selecSort {
+    public static void selectionSort(int[] arr){
+        int ind = -1;
+        for(int i = 0 ;  i<arr.length -1 ; i++){
+            int m = arr[i];
+            //int ind = -1;
+            for(int j = i ; j<arr.length ; j++){
+                if(arr[j]<m){
+                    m=arr[j];
+                    ind = j;
+                }
+            }
+            arr[ind] = arr[i];
+            arr[i] = m;
+
+        }
+    }
+    public static void main(String[] args) {
+        int[] ar = {8,6,5,3,2,1,0};
+        selectionSort(ar);
+        for(int i  = 0; i<ar.length ; i++){
+            System.out.print(ar[i] + " ");
+        }
+    }
+}
